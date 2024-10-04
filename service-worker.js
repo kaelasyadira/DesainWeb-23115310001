4a9ec094e3f95bb85cdd78c09bea90273cf86dac
kaelasyadira <146148661+kaelasyadira@users.noreply.github.com>
2024-10-04 12:10:21 +0700

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -4,8 +4,8 @@ const cache_assets = [
     'contact.html',
     'about.html',
     'offline.html',
-    './apps.js',
-    './styles.css'
+    'apps.js',
+    'styles.css'
 ];
 
 // Install event
@@ -43,31 +43,32 @@ self.addEventListener('activate', (event) => {
 });
 
 // Fetch event
-// Fetch event
 self.addEventListener('fetch', (event) => {
-    console.log('Service Worker: Fetching');
-    event.respondWith(
-        caches.match(event.request).then(
-            (response) => {
-                // Jika ada respons di cache, kembalikan respons tersebut
-                if (response) {
-                    return response;
-                }
-                // Jika tidak ada di cache, lakukan fetch dari jaringan
-                return fetch(event.request).then(
-                    (fetchResponse) => {
-                        return caches.open(cache_name).then(
-                            (cache) => {
-                                // Simpan respons ke dalam cache untuk digunakan di masa mendatang
-                                cache.put(event.request, fetchResponse.clone());
-                                return fetchResponse; // Kembalikan respons asli
-                            }
-                        );
-                    }
-                ).catch(
-                    () => caches.match('offline.html') // Jika fetch gagal, tampilkan halaman offline
-                );
-            }
-        )
-    );
+  console.log('Service Worker: Fetching');
+  event.respondWith(
+      caches.match(event.request).then(
+          (response) => {
+              if (response) {
+                  return response; // Kembalikan respons dari cache
+              }
+              return fetch(event.request).then(
+                  (fetchResponse) => {
+                      // Hanya simpan respons jika statusnya 200
+                      if (!fetchResponse || fetchResponse.status !== 200) {
+                          return fetchResponse; // Kembalikan respons asli jika bukan 200
+                      }
+                      return caches.open(cache_name).then(
+                          (cache) => {
+                              cache.put(event.request, fetchResponse.clone());
+                              return fetchResponse; // Kembalikan respons asli
+                          }
+                      );
+                  }
+              ).catch(
+                  () => caches.match('offline.html') // Menampilkan halaman offline jika fetch gagal
+              );
+          }
+      )
+  );
 });
+
