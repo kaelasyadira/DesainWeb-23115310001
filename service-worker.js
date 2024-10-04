2f6661d9db0bd2bfa47862b88572d56b425d257a
kaelasyadira <146148661+kaelasyadira@users.noreply.github.com>
2024-10-04 11:20:10 +0700

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,60 @@
+const cache_name = 'v1';
+const cache_assets = [
+    'index.html',
+    'contact.html',
+    'about.html',
+    'offline.html',
+    './apps.js',
+    './styles.css'
+];
+
+// Install event
+self.addEventListener('install', (event) => {
+    console.log('Service Worker: Installed');
+    event.waitUntil(
+        caches.open(cache_name).then(
+            (cache) => {
+                console.log('Service Worker: Caching Files');
+                return cache.addAll(cache_assets);  // Tambahkan "return" untuk memastikan proses selesai
+            }
+        )
+    );
+});
+
+// Activate event
+self.addEventListener('activate', (event) => {
+    console.log('Service Worker: Activated');
+    event.waitUntil(
+        caches.keys().then(
+            (cacheNames) => {
+                return Promise.all(
+                    cacheNames.map(
+                        (cache) => {
+                            if (cache !== cache_name) {
+                                console.log('Service Worker: Clearing Old Cache');
+                                return caches.delete(cache);
+                            }
+                        }
+                    )
+                );
+            }
+        )
+    );
+});
+
+// Fetch event
+self.addEventListener('fetch', (event) => {
+    console.log('Service Worker: Fetching');
+    event.respondWith(
+        caches.match(event.request).then(
+            (response) => {
+                if (response) {
+                    return response;
+                }
+                return fetch(event.request).catch(
+                    () => caches.match('offline.html')
+                );
+            }
+        )
+    );
+});
