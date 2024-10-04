873e22424ef894b0264f30ecbdfcf63376f7208f
kaelasyadira <146148661+kaelasyadira@users.noreply.github.com>
2024-10-04 12:13:49 +0700

    Update apps.js

--- apps.js
+++ apps.js
@@ -1,13 +1,11 @@
 if ('serviceWorker' in navigator) {
     window.addEventListener('load', () => {
-        navigator.serviceWorker.register('./service-worker.js') // Ganti '/' dengan './' jika file ada di root.
-        .then(registration => {
-            console.log('Service Worker registered with scope:', registration.scope);
-        })
-        .catch(error => {
-            console.error('Service Worker registration failed:', error);
-        });
+        navigator.serviceWorker.register('./service-worker.js') // Ganti dengan path yang sesuai
+            .then(registration => {
+                console.log('Service Worker registered with scope:', registration.scope);
+            })
+            .catch(error => {
+                console.log('Service Worker registration failed:', error);
+            });
     });
-} else {
-    console.warn('Service Workers are not supported in this browser.');
 }
